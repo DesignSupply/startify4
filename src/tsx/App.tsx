cc07af6c4983db162d23b261dbc910206f04da0d
DesignSupply <ogawa@designsupply-web.com>
2023-06-10 03:39:51 +0900

    [add] Reactコンポーネント追加

---
+++ src/tsx/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>Startify 4</h1>
+    </>
+  )
+}
+
+export default App;
---
+++ src/tsx/main.tsx
@@ -0,0 +1,17 @@
+import '../scss/main.scss';
+import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '../tsx/App';
+
+// React
+ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
+  <React.StrictMode>  
+    <App />
+  </React.StrictMode>
+)
+
+// jQuery
+$(function () {
+  console.log('jQuery is ready.');
+});
