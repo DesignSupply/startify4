60872fcb09840ee7522c7d41cc961830c30e7548
DesignSupply <ogawa@designsupply-web.com>
2023-06-13 01:27:57 +0900

    [add] React Routerの対応

--- src/tsx/App.tsx
+++ src/tsx/App.tsx
@@ -1,10 +1,21 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import ExampleComponent from './components/ExampleComponent';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
 
+// store
 const storeData = {
   message: 'Hello World'
 }
 const SampleContext = createContext({});
+
+// route
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+]);
 
 const App: React.FC = () => {
   const [text, setText] = useState('ExampleComponent');
@@ -16,6 +27,7 @@ const App: React.FC = () => {
   return (
     <>
       <SampleContext.Provider value={{context, setContext}}>
+        <RouterProvider router={router} />
         <ExampleComponent message={text} />
       </SampleContext.Provider>
     </>
